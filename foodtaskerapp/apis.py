d50412a776a043445c01e50f38fc4439fc61231e
daniel <daniel_cleaves@hotmail.com>
2017-01-08 14:06:15 -0600

    Added API dor drivers

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -9,6 +9,11 @@ from oauth2_provider.models import AccessToken
 from foodtaskerapp.models import Restaurant, Meal, Order, OrderDetails
 from foodtaskerapp.serializers import RestaurantSerializer, MealSerializer, OrderSerializer
 
+
+#########
+# Customers
+#########
+
 def customer_get_restaurants(request):
 	restaurants = RestaurantSerializer(
 		Restaurant.objects.all().order_by("-id"),
@@ -96,6 +101,9 @@ def customer_get_latest_order(request):
 
 	return JsonResponse({"order": order})
 
+#########
+# Restaurant
+#########
 
 def restaurant_order_notification(request, last_request_time):
 	notification = Order.objects.filter(restaurant = request.user.restaurant, 
@@ -103,3 +111,104 @@ def restaurant_order_notification(request, last_request_time):
 
 	return JsonResponse({"notification": notification})
 
+
+#########
+# Driver
+#########
+
+def driver_get_ready_orders(request):
+	orders = OrderSerializer(
+		Order.objects.filter(status = Order.READY, driver = None).order_by("-id"), 
+		many = True
+	).data
+	return JsonResponse({"orders": orders})
+
+@csrf_exempt
+# POST params: access_token, order_id
+def driver_pick_order(request):
+
+	if request.method == "POST":
+		# Get Token
+		access_token = AccessToken.objects.get(token = request.POST.get("access_token"), 
+			expires__gt = timezone.now())
+
+		# Get Driver
+		driver = access_token.user.driver
+
+		# Check if driver can only pick up one order at the same time
+		if Order.objects.filter(driver = driver).exclude(status = Order.ONTHEWAY):
+			return JsonResponse({"status": "failed", "error": "You can only pick up one order at the same time."})
+
+		try: 
+			order = Order.objects.get(
+				id = request.POST["order_id"],
+				driver = None, 
+				status = Order.READY 
+			)
+			order.driver = driver
+			order.status = Order.ONTHEWAY
+			order.picked_at = timezone.now()
+			order.save()
+
+			return JsonResponse({"status": "success"})
+
+		except Order.DOESNOTExist:
+			return JsonResponse({"status": "failed", "error": "This order has been picked up by another driver."})
+
+
+
+	return JsonResponse({})
+
+# Get params: access_token
+def driver_get_latest_order(request):
+# Get Token
+	access_token = AccessToken.objects.get(token = request.GET.get("access_token"), 
+		expires__gt = timezone.now())
+
+	driver = access_token.user.driver
+	order = OrderSerializer(
+		Order.objects.filter(driver = driver).order_by("picked_at").last()
+	).data
+	return JsonResponse({"order": order})
+
+# POST Params: access_token, order_id
+@csrf_exempt
+def driver_complete_order(request):
+# Get Token
+	access_token = AccessToken.objects.get(token = request.POST.get("access_token"), 
+		expires__gt = timezone.now())
+
+	driver = access_token.user.driver
+
+	order = Order.objects.get(id = request.POST["order_id"], driver = driver)
+	order.status = Order.DELIVERED
+	order.save()
+
+	return JsonResponse({"status": "success"})
+
+# GET params: access_token
+def driver_get_revenue(request):
+# Get Token
+	access_token = AccessToken.objects.get(token = request.GET.get("access_token"), 
+		expires__gt = timezone.now())
+
+	driver = access_token.user.driver
+
+	from datetime import timedelta
+
+	revenue = {}
+	today = timezone.now()
+	current_weekdays = [today + timedelta(days = i) for i in range(0 - today.weekday(), 7 - today.weekday())]
+
+	for day in current_weekdays:
+		orders = Order.objects.filter(
+			driver = driver,
+			status = Order.DELIVERED,
+			created_at__year = day.year,
+			created_at__month = day.month,
+			created_at__day = day.day,
+		)
+
+		revenue[day.strftime("%a")] = sum(order.total for order in orders)
+
+	return JsonResponse({"revenue": revenue})
